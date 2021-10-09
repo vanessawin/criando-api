2762174fbeb304110096a09a944094f570e3b739
vanessawin <vanessa.win@hotmail.com>
2021-10-09 16:43:43 -0300

    Intalando o npm uuid

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid') //importando o modulo para gerar o id
+
 const app = express()
 app.use(express.json()) // passando uma configuração para o express, 
 //para parsear todas as minhas entradas das apis para json
@@ -17,27 +19,31 @@ app.get('/notes', (req, res) => { //adicione depois da / notes na rota
 
 // Metodo Post
 //Altere o metodo get para Post
-app.post('/notes', (req, res) => { 
-  const title = req.body.title 
-  const description = req.body.description 
+app.post('/notes', (req, res) => {
+  const title = req.body.title
+  const description = req.body.description
 
   // Validando o campo title
   // Verificar se o campo title não estiver preenchido 
-  if(!title) return res.status(400).json({ mensage: "Informe o campo title"})
+  if (!title) return res.status(400).json({ mensage: "Informe o campo title" })
 
   // Validando o campo description
   // Verificar se o campo description não estiver preenchido 
-  if(!description) return res.status(400).json({ mensage: "Informe o campo description"})
-  
+  if (!description) return res.status(400).json({ mensage: "Informe o campo description" })
+
   //Inserindo as informações dentro do notes
-  notes.push({title,description}) // como o campo e o valor tem o mesmo nome só precisamos colocar o mesmo nome
-  
+  notes.push({
+    id: uuidv4(), //chamando a função do gerador de id
+    title,
+    description
+  }) // como o campo e o valor tem o mesmo nome só precisamos colocar o mesmo nome
+
   // //caso contrario seria assim 
   // notes.push({
   //   title: title,
   //   description: description
   // })
-  res.json({ menssage: "Anotação salva com sucesso!"}) 
+  res.json({ menssage: "Anotação salva com sucesso!" })
 })
 
 app.listen(port, () => {
