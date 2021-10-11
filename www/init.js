94e7bb4eb4aebd91c7031020705e56cddceaead1
vanessawin <vanessa.win@hotmail.com>
2021-10-11 15:03:48 -0300

    inserindo ajax/jquery/criando a função para salvar

---
+++ www/init.js
@@ -0,0 +1,6 @@
+function salvar (){    // função  para o botão salvar
+    const titulo = $('#titulo').val() // pegando o valor do campo titulo com jquery
+    const descricao = $('#descricao').val() // pegando o valor do campo descrição com jquery
+
+    console.log(titulo, descricao)
+}
